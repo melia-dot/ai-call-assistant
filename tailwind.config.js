d102c0f42f6b32dce8903b1f55aec4058c6b34ba
Mikael Eliaeson <melia-dot@users.noreply.github.com>
2025-09-05 09:16:41 +0100

    Fix Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,7 +5,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
   },
   plugins: [],
 }
